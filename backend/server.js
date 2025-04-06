7f1bdee12d8dbbca66a791a49d5ced5b0f16ae65
priyanshu0511 <priyanshu05112@gmail.com>
2025-04-06 14:23:16 +0530

    added frontend url

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ require("dotenv").config();
 
 const app= new express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+  }));
 app.use(express.json())
 
 app.get('/',(req,res)=>{
